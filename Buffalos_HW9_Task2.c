7218eda215e8069a214637a10273929c0f830788
Tysen Bair <tysenbair@mail.weber.edu>
2017-04-06 15:28:39 +0000

    initial

---
+++ Buffalos_HW9_Task2.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Buffalos_HW9_Task2.c
+ *       	Usage:  ./Buffalos_HW9_Task2.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/06/2017 03:27:13 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Tysen Bair (), tysenbair@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
